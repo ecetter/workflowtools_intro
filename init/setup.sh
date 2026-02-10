1dcbb056d7a162692438039acb16e81b43502fb6
ecetter <ecetter@gmail.com>
2026-02-10 11:43:46 -0500

    include conda environment setup commands

--- init/setup.sh
+++ init/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# load anaconda from software stack
 module load anaconda/2023
 module list
 
+# create environment that contains snakemake and its dependencies
+conda create -y -n workflow_env
+conda activate workflow_env
+conda install -y snakemake snakemake-executor-plugin-slurm
